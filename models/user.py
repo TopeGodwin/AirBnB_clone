57068b4d935e936d6c571695c3e4f1ae36955492
TopeGodwin <jimohtope2012@gmail.com>
2023-05-20 03:11:49 -0700

    web static

--- models/user.py
+++ models/user.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""user class, subclass of BaseModel
+"""
+
 from models.base_model import BaseModel
+import json
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
+    '''subclass of BaseModel class'''
 
     email = ""
     password = ""
